347382a873d2e4a4c4a401b1b50ac0976637d465
radu.chivereanu <radu.chivereanu@fep8.grid.pub.ro>
2022-04-29 18:27:55 +0300

    Blas + memory + cache

--- src/solver_blas.c
+++ src/solver_blas.c
@@ -3,8 +3,29 @@
  * 2022 Spring
  */
 #include "utils.h"
-
+#include "cblas.h"
 /*
  * Add your BLAS implementation here
  */
-double *my_solver(int N, double *A, double *B) { return NULL; }
+
+double *my_solver(int N, double *A, double *B) {
+        cblas_dtrmm(CblasRowMajor,
+		   CblasRight,
+		   CblasUpper,
+		   CblasTrans,
+		   CblasNonUnit,
+		    N, N, 1, A, N, A, N);
+	
+	double *C = malloc(N * N * sizeof(double));
+	cblas_dgemm(CblasRowMajor,
+		    CblasTrans,
+		    CblasNoTrans,
+		    N, N, N, 1, B, N, B, N, 0, C, N); 
+
+	cblas_dsymm(CblasRowMajor,
+		    CblasRight,
+		    CblasUpper,
+		    N, N, 1, A, N, B, N, 1, C, N);
+	       
+	return C;
+}
